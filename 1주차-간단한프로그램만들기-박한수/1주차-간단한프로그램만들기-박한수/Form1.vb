12b75b264d51231fb8aa53a485bf77e9036ca980
Hansoo Park <mnlk93232671@gmail.com>
2022-04-14 15:59:38 +0900

    Delete 1주차-간단한프로그램만들기-박한수 directory

--- 1주차-간단한프로그램만들기-박한수/1주차-간단한프로그램만들기-박한수/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("비주얼베이직 세상에 오신 것을 환영합니다^^", "여기가 간판   ")
-    End Sub
-End Class
